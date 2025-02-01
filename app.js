d8ac5edd3e2626da28566588b8242de9d15c9b12
matheus bandeira <matheus.bandeiracosta@gmail.com>
2025-01-31 22:13:03 -0300

    diversas mudancas

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigoLista=[];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value;
+
+    if(nome===''){
+        alert('Insira um nome valido');
+    }else{
+        amigoLista.push(nome);
+    }
+    limparCampo();
+}
+
+function limparCampo(){
+    nome = document.getElementById('amigo');
+    nome.value = '';
+}
+function exibirTexto(id, Texto){
+    let campo = document.querySelector(id);
+    campo.innerHTML = Texto;
+}
+
+function listaAmigos() {
+    exibirTexto(`listaAmigos, ${nome}`);
+}
+
+function sortearAmigo() {
+    let quatElementLista=amigoLista.length;
+    
+    if(quatElementLista == 100){
+        amigoLista =[];
+    }
+
+    let nomeEscolhido = parseInt(Math.random() *amigoLista.length);
+    if(amigoLista.includes(nomeEscolhido)){
+        return sortearAmigo();
+
+       }else{
+        amigoLista.push(nomeEscolhido);
+        console.log(amigoLista);
+        console.log(nomeEscolhido);
+       }
+       
+}
